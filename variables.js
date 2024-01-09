9b11e5b34b05a2c2f512a74e7087b17b7c4b6e32
andyvaper <andyvazquezperez@hotmail.com>
2024-01-08 18:40:43 -0600

    variables js

---
+++ variables.js
@@ -0,0 +1,29 @@
+var edadTomas = 22;  
+var altura = 1.75;
+var year = 2001;
+let suma = 22 + 2001
+console.log + suma
+
+var nombre = "Tomas";
+let ciudad = 'Puebla';
+const saludo = `Hola, ${nombre}!`;
+
+let esMayorDeEdad = edad = 22; true;
+const tieneMascota = true;
+var llueve = false;
+let hobbiesFav = ["Leer", "Bailar", "Videojuegos",]
+
+let peliculaFav = {
+    nombre: "La la land",
+    director: "Damien Chazelle",
+    año: "2016",
+    genero: "Musical",
+};
+
+const NOMBRE_USUARIO = "Tomas"
+var nombre = Tomas;
+
+var nombre = Andres 
+(false);
+
+let datoVacio = null;
